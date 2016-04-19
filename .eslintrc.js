143d593d51ec4ba2929f4dffd855b28cfb399adb
heidiflammable <heidiflammable@gmail.com>
2016-04-19 06:51:57 -0700

    adding final eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,61 +1,69 @@
 module.exports = {
-    "env": {
-        "browser": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "comma-dangle": [
-            "error",
-            "never",
-        ],
-        "no-console": [
-            "error",
-        ],
-        "max-len": [
-            "error",
-            80, 
-            {"ignoreUrls": true}
-        ],
-        "curly": [ 
-            "error",
-        ],
-        "object-curly-spacing": [
-            "error", 
-            "always"
-        ],
-        "no-trailing-spaces": [
-            "error",
-        ],
-        "eqeqeq": [
-            "error", 
-            "allow-null"
-        ],
-        "newline-before-return": [
-            "error"
-        ],
-        "no-undef": [//no undeclared vars unless global
-            "error"
-        ],
-        "no-unused-vars": [
-            "error"
-        ]
-    }
+		"env": {
+				"browser": true
+		},
+
+		//specifying global var google
+		"globals": {
+				"google": true,
+				"result": true,
+				"round": true,
+				"module": true
+		},
+		"extends": "eslint:recommended",
+		"rules": {
+				"indent": [
+						"error",
+						2
+				],
+				"quotes": [
+						"error",
+						"single"
+				],
+				"semi": [
+						"error",
+						"always"
+				],
+				"comma-dangle": [
+						"error",
+						"never",
+				],
+				"no-console": [
+						"error",
+				],
+				"max-len": [
+						"error",
+						80, 2,
+						{"ignoreUrls": true}
+				],
+				"curly": [
+						"error",
+				],
+				"object-curly-spacing": [
+						"error",
+						"always"
+				],
+				"no-trailing-spaces": [
+						"error",
+				],
+				"eqeqeq": [
+						"error",
+						"allow-null"
+				],
+				"newline-before-return": [
+						"error"
+				],
+
+				//no undeclared vars unless global
+				"no-undef": [
+						"error"
+				],
+				"no-unused-vars": [
+						"error"
+				],
+				"strict": [
+					"error",
+					"global"
+				]
+		}
 };
-
-
